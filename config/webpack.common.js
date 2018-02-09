e3e41341396f3575e7c676399ad3715cbf84515b
Arias <489291461@qq.com>
2018-02-09 11:36:14 +0800

    实现自动刷新

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry: './src/index.ts',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            inject: true
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '..', 'dist')
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
     module: {
         rules: [
             {
@@ -21,18 +34,5 @@ module.exports = {
             }
         ]
     },
-    devtool: "source-map",
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new UglifyJSPlugin({
-            sourceMap: true
-        })
-    ],
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-};
+    target: 'electron-renderer'
+}
